127f04492e9b2fc15b1e84f7ede3c4f61a6f94f6
Samrullo <amrulloev.subhon@gmail.com>
2024-11-15 18:07:45 +0900

    Updates

---
+++ precache-manifest.4a16e5bfd965169349f74854e6c9061b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2ae1a9d9caf822dcc847b863945e42",
+    "url": "/uzbek-nasr/index.html"
+  },
+  {
+    "revision": "40fc23b96099ac771241",
+    "url": "/uzbek-nasr/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "b216eb7cfa125fcd9499",
+    "url": "/uzbek-nasr/static/css/main.190453b1.chunk.css"
+  },
+  {
+    "revision": "40fc23b96099ac771241",
+    "url": "/uzbek-nasr/static/js/2.3a6019aa.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/uzbek-nasr/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "b216eb7cfa125fcd9499",
+    "url": "/uzbek-nasr/static/js/main.f99f87a1.chunk.js"
+  },
+  {
+    "revision": "8a420371562179fc79ee",
+    "url": "/uzbek-nasr/static/js/runtime~main.79853b2c.js"
+  }
+]);
