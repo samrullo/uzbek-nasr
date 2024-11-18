6ebed6b9fd5809187793e12abbd627f8f096dabe
Samrullo <amrulloev.subhon@gmail.com>
2024-11-18 12:05:31 +0900

    commit uzbek-nasr gh-pages branch from build directory

---
+++ precache-manifest.2b8f276e4fcb68ec218b2ff5352f8db0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9816716c30a488b3f438b47475cfd71c",
+    "url": "/uzbek-nasr/index.html"
+  },
+  {
+    "revision": "40fc23b96099ac771241",
+    "url": "/uzbek-nasr/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "23f76cb2c0ea7fd8122e",
+    "url": "/uzbek-nasr/static/css/main.190453b1.chunk.css"
+  },
+  {
+    "revision": "40fc23b96099ac771241",
+    "url": "/uzbek-nasr/static/js/2.3a6019aa.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/uzbek-nasr/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "23f76cb2c0ea7fd8122e",
+    "url": "/uzbek-nasr/static/js/main.73e7ebad.chunk.js"
+  },
+  {
+    "revision": "8a420371562179fc79ee",
+    "url": "/uzbek-nasr/static/js/runtime~main.79853b2c.js"
+  }
+]);
