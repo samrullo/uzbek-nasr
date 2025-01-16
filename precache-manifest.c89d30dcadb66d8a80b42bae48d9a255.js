950627cfa26a85fac6036a38a51a4d4910eba445
Subkhon Amrulloev <amrulloev.suhon@gmail.com>
2025-01-16 20:22:27 +0900

    uzbek nasr gh-pages branch

---
+++ precache-manifest.c89d30dcadb66d8a80b42bae48d9a255.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9459163e634df604a062712f94e67d2d",
+    "url": "/uzbek-nasr/index.html"
+  },
+  {
+    "revision": "e48f5780b79aaa373695",
+    "url": "/uzbek-nasr/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "e33e9988a93cdb371399",
+    "url": "/uzbek-nasr/static/css/main.6ca0b154.chunk.css"
+  },
+  {
+    "revision": "e48f5780b79aaa373695",
+    "url": "/uzbek-nasr/static/js/2.7c203245.chunk.js"
+  },
+  {
+    "revision": "3ab4a5df061cabbdae3bf41226f486bc",
+    "url": "/uzbek-nasr/static/js/2.7c203245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e749f2a8ed2486b47c",
+    "url": "/uzbek-nasr/static/js/3.daa0c00a.chunk.js"
+  },
+  {
+    "revision": "e33e9988a93cdb371399",
+    "url": "/uzbek-nasr/static/js/main.39caae55.chunk.js"
+  },
+  {
+    "revision": "247cfd0b1aa5a7b6988e",
+    "url": "/uzbek-nasr/static/js/runtime-main.2a2dcbf6.js"
+  }
+]);
